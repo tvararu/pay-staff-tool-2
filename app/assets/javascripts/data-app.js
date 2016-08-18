5d7d68a53475028dd09f9d04cc4cd44a15a99f44
Colm Britton <colmjude@gmail.com>
2016-08-18 10:35:23 +0100

    add mapping function, row to data object

--- app/assets/javascripts/data-app.js
+++ app/assets/javascripts/data-app.js
@@ -22,6 +22,35 @@
   }
 
   /**
+   * Map all the cells from a row in the spreadsheet
+   * into a data object that the templates can use
+   */
+  function mapRowToObject(row, rowNo) {
+    var obj = {
+      "row": rowNo,
+      "reference": row[0],
+      "firstName": row[1],
+      "lastName": row[2],
+      "email": row[3],
+      "amount": row[4],
+      "status": row[5],
+      "subStatus": row[6],
+      "card": row[7],
+      "provider": row[8],
+      "gatewayId": row[9],
+      "payId": row[10],
+      "startDate": formatDate(new Date ( Date.parse(row[11]) ) ),
+      "startEnter": formatDate(new Date ( Date.parse(row[12]) ) ),
+      "authSubmit": formatDate(new Date ( Date.parse(row[13]) ) ),
+      "authSucceed": formatDate(new Date ( Date.parse(row[14]) ) ),
+      "paySubmit": formatDate(new Date ( Date.parse(row[15]) ) ),
+      "paySucceed": formatDate(new Date ( Date.parse(row[16]) ) ),
+      "failed": formatDate(new Date ( Date.parse(row[17]) ) )
+    };
+    return obj;
+  }
+
+  /**
    * Print the names and majors of students in a sample spreadsheet:
    * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
    */
@@ -33,20 +62,12 @@
       $("body").addClass("gs-data-loaded");
       var range = response.result;
       if (range.values.length > 0) {
-        console.log(range.values[0]);
+
         for (i = 0; i < range.values.length; i++) {
           var row = range.values[i];
-          var item = {
-            "row": i + 1,
-            "reference": row[0],
-            "email": row[3],
-            "amount": row[4],
-            "card": row[7],
-            "status": row[5],
-            "subStatus": row[6],
-            "startDate": formatDate(new Date ( Date.parse(row[11]) ) )
-          };
-          // Print columns A and E, which correspond to indices 0 and 4.
+          var item = mapRowToObject(row, i+1);
+
+          // call render function
           appendPre(item);
         }
       } else {
